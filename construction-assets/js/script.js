40b886572a1077ddbd4b6cdcbf149de74392c920
Sasha <sasha@xucova.com>
2019-03-31 12:07:39 +1100

    init

---
+++ construction-assets/js/script.js
@@ -0,0 +1,6 @@
+var swiper = new Swiper('.main_slider_construction', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+});
